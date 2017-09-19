cb42a788772540f4809f07e6fa194ce7fb1ce014
choi rak jun <choirak0805@google.com>
2017-09-19 16:42:22 +0900

    탄막슈팅게임_비행기추가 및 좌,우,상,위 키보드 조작에 맞춰 이동.

---
+++ 오픈소스/Assets/Scripts/move.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    
+    public float move_sp = 0.09f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            transform.Translate(move_sp, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            transform.Translate(-move_sp, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            transform.Translate(0, move_sp, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+            transform.Translate(0, -move_sp, 0);
+        }
+    }
+}
+
