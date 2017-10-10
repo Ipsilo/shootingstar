bcdc08750ec4a46ce02dbcc401f41ef21066fca5
DESKTOP-KL9TUT2\최원준 <hhh9597h@naver.com>
2017-10-10 16:42:05 +0900

    무한 스크롤 배경 추가

---
+++ Assets/Scripts/background_move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_move : MonoBehaviour {
+    public float ScrollSpeed = 1.0f;
+    float Offset;
+    private new Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Offset += Time.deltaTime * ScrollSpeed;
+        renderer.material.mainTextureOffset = new Vector2(0, Offset);
+	}
+}
--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-
-    
     public float move_sp = 0.09f;
     // Use this for initialization
     void Start()
@@ -16,30 +14,26 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
         if (Input.GetKey(KeyCode.RightArrow))
         {
-
             transform.Translate(move_sp, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-
             transform.Translate(-move_sp, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-
             transform.Translate(0, move_sp, 0);
         }
+
         if (Input.GetKey(KeyCode.DownArrow))
         {
-
             transform.Translate(0, -move_sp, 0);
         }
+        
     }
 }
 
