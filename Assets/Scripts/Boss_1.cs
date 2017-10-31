0ae9ee99129dd422443842e25169ac66891ada47
DESKTOP-RQSHB4M\pow89 <pow8956@naver.com>
2017-10-31 16:36:57 +0900

    1스테이지보스 1패턴 수정 및 2패턴 구현

--- Assets/Scripts/Boss_1.cs
+++ Assets/Scripts/Boss_1.cs
@@ -2,27 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss_1 : MonoBehaviour {
-    public int BossHP;
-    public int Phase2HP;
-    public int Phase3HP;
+public class Boss_1 : MonoBehaviour
+{
+    public int bossHP;
+    public int phase2HP;
+    public int phase3HP;
     private Boss_Data bossData;
-    public float Shot;
-    public float Speed;
+    public float shot;
+    public float speed;
     public GameObject bullet;
-    public GameObject Boss1;
-    public float ShotAngle;
-    private float Angle;
-    public int rotate;
+    public GameObject boss1;
+    public GameObject plane;
+    public float shotAngle;
+    public float shotAngleRate;
+    private float angle;
+    private int rotate;
 
-	// Use this for initialization
-	void Start () {
-        bossData = new Boss_Data(BossHP);
+    // Use this for initialization
+    void Start()
+    {
+        bossData = new Boss_Data(bossHP);
         Debug.Log(gameObject.name + "의 체력: " + bossData.getHP());
-        Angle = 0;
+        angle = 0;
         rotate = 0;
-        StartCoroutine(BulletSpell1());
-        if (bossData.getHP() < Phase2HP)
+
+        StartCoroutine(BulletSpell3());
+        if (bossData.getHP() < phase2HP)
         {
             StartCoroutine(BulletSpell2());
         }
@@ -36,58 +41,66 @@ public class Boss_1 : MonoBehaviour {
     {
         do
         {
-            for (int i = 0; i < Shot; i++)
+            for (int i = 0; i < shot; i++)
             {
                 Debug.Log(i);
                 GameObject obj;
-                obj = (GameObject)Instantiate(bullet, Boss1.transform.position, Quaternion.identity);
+                //탄 생성
+                obj = (GameObject)Instantiate(bullet, boss1.transform.position, Quaternion.identity);
 
-                //보스의 위치에 bullet을 생성합니다.
-                obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(Speed * Mathf.Cos(Mathf.PI * 2 * i / Shot), Speed * Mathf.Sin(Mathf.PI * i * 2 / Shot)));
+                //bullet을 원형으로 발사
+                obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed * Mathf.Cos(Mathf.PI * 2 * i / shot), speed * Mathf.Sin(Mathf.PI * i * 2 / shot)));
             }
+            //탄 발사 간격
             yield return new WaitForSeconds(1.2f);
 
-            for (int i = 0; i < Shot; i++)
+            for (int i = 0; i < shot; i++)
             {
                 Debug.Log(i);
                 GameObject obj;
-                obj = (GameObject)Instantiate(bullet, Boss1.transform.position, Quaternion.identity);
+                obj = (GameObject)Instantiate(bullet, boss1.transform.position, Quaternion.identity);
 
-                //보스의 위치에 bullet을 생성합니다.
-                obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(Speed * Mathf.Cos(Mathf.PI * 2 * i / Shot + Mathf.PI / 2), Speed * Mathf.Sin(Mathf.PI * i * 2 / Shot + Mathf.PI / 2)));
+                //bullet생성 위치를 바꾸고 원형으로 발사
+                obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed * Mathf.Cos(Mathf.PI * 2 * i / shot + Mathf.PI / 2), speed * Mathf.Sin(Mathf.PI * i * 2 / shot + Mathf.PI / 2)));
             }
+            //탄 발사 간격
             yield return new WaitForSeconds(1.2f);
         } while (true);
     }
+
     IEnumerator BulletSpell2()
     {
         do
         {
-            if (Angle >= 2)
-            {
-                Angle = 0;
-            }
             GameObject obj;
-            obj = Instantiate(bullet, Boss1.transform.position, Quaternion.identity);
+            obj = Instantiate(bullet, boss1.transform.position, Quaternion.identity);
 
-            //보스의 위치에 bullet을 생성
-            obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(Speed * Mathf.Cos(Mathf.PI * Angle), Speed * Mathf.Sin(Mathf.PI * Angle)));
+            //bullet을 생성
+            obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed * Mathf.Cos(Mathf.PI * angle), speed * Mathf.Sin(Mathf.PI * angle)));
 
-            Angle += ShotAngle;
-
+            if (angle > 2)
+            {
+                angle = 0;
+            }
+            if (angle < 2)
+            {
+                angle += shotAngle;
+            }
             if (rotate == 0)
             {
-                ShotAngle += ShotAngle/100;
-                if (ShotAngle > 0.7)
+                shotAngle += shotAngleRate;
+                shotAngleRate += shotAngleRate / 100;
+                if (shotAngle > 0.999)
                 {
                     rotate = 1;
                 }
             }
 
-            if(rotate == 1)
+            if (rotate == 1)
             {
-                ShotAngle -= ShotAngle/100;
-                if(ShotAngle < 0.1)
+                shotAngle -= shotAngleRate;
+                shotAngleRate -= shotAngleRate / 100;
+                if (shotAngle < 0.1)
                 {
                     rotate = 0;
                 }
@@ -97,5 +110,30 @@ public class Boss_1 : MonoBehaviour {
         } while (true);
     }
 
+    IEnumerator BulletSpell3()
+    {
+        Rigidbody2D temp;
+        GameObject obj;
+        Vector2 v;
+        do
+        {
+
+            for (int i = 0; i < shot; i++)
+            {
+                Debug.Log(i);
+                //탄 생성
+                obj = (GameObject)Instantiate(bullet, boss1.transform.position, Quaternion.identity);
+
+                //bullet을 원형으로 생성
+                //obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed * Mathf.Cos(Mathf.PI * 2 * i / shot), speed * Mathf.Sin(Mathf.PI * i * 2 / shot)));
+
+                v = plane.transform.position;
+                obj.GetComponent<Rigidbody2D>().AddForce(v.normalized * speed);
+            }
+            yield return new WaitForSeconds(0.1f);
+        } while (true);
+    }
 }
+
+
 
