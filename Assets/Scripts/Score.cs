888b9f7aba61626e65bdd0e337d83a0e13aee9d2
choi rak jun <choirak0805@google.com>
2017-11-16 15:38:31 +0900

    체력바, 체력 감소 코드 작성. 아직 적 미사일과 보스 미사일과의 충돌 시 체력 감소 시험 못함. 게임 배경(quad,player객체를 canvas로 모아넣는 등의 수정 필요)

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    public Slider healthBarSlider;//slider 참조
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void OnTriggerStay(Collider other)
+    {
+        if (((other.gameObject.name == "bullet")|| (other.gameObject.name == "Enemy")&& healthBarSlider.value > 0)){
+            healthBarSlider.value -= 10;
+        }
+    }
+}
