669241c347b8f63e127261550aa0c9d9d7911a75
choi rak jun <choirak0805@google.com>
2017-11-21 16:26:34 +0900

    Player체력설정 및 체력바(slider)설정

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+
+
+public class DamagePlayer : MonoBehaviour {
+    public int playerHealth = 30;
+    int damage = 10;
+
+    void start()
+    {
+        print(playerHealth);
+
+    }
+
+    void OnCollisionEnter(Collision _colision)
+    {
+        if (_colision.gameObject.tag == "Enemy")
+        {
+            playerHealth -= damage;
+            print("enemy just touched something weird");
+        }
+    }
+
+}
