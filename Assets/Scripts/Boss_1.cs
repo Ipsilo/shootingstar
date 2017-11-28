6505228dda538adb0fa4a8e456244440f995c9a0
DESKTOP-RQSHB4M\pow89 <pow8956@naver.com>
2017-11-28 16:50:12 +0900

    주석추가, 패턴코드 수정

--- Assets/Scripts/Boss_1.cs
+++ Assets/Scripts/Boss_1.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class Boss_1 : MonoBehaviour
 {
     private Boss_Data bossData;
-    public int bossHP;
-    public int phase2HP;
-    public int phase3HP;
-    public float shot;
-    public float speed;
+    public int bossHP;      //보스 총HP
+    public int phase2HP;    //보스 제 2패턴 시작HP
+    public int phase3HP;    //보스 제 3패턴 시작HP
+    public float shot;      //1패턴 발사 미사일 개수
+    public float speed;     //탄이 날라가는 속도
     public GameObject bullet;
     public GameObject boss1;
     public GameObject plane;
     public GameObject explosion;
     public float shotAngle;
     public float shotAngleRate;
-    private int level;
+    private int level;      //패턴 시작 조건 
     Coroutine spell1;
     Coroutine spell2;
     Coroutine spell3;
@@ -40,26 +40,26 @@ public class Boss_1 : MonoBehaviour
             level++;
         }
         //1패턴 종료후 2패턴 시작
-        if (bossData.getHP() < phase2HP && level == 2)
+        if (bossData.getHP() <= phase2HP && level == 2)
         {
             StopCoroutine(spell1);
             spell2 = StartCoroutine(BulletSpell2());
             level++;
         }
         //2패턴 종료후 1,3패턴 시작
-        if (bossData.getHP() < phase3HP && level == 3)
+        if (bossData.getHP() <= phase3HP && level == 3)
         {
             StopCoroutine(spell2);
             spell3 = StartCoroutine(BulletSpell1());
             spell4 = StartCoroutine(BulletSpell3());
             level++;
         }
-        //체력이 0보다 낮으면 1,3패턴 종료후 오브젝트  제거
+        //체력이 0이 되면 1,3패턴 종료후 오브젝트  제거
         if (bossData.getHP() <= 0)
         {
             StopCoroutine(spell3);
             StopCoroutine(spell4);
-            Instantiate(explosion,boss1.transform.position,Quaternion.identity);
+            Instantiate(explosion,boss1.transform.position,Quaternion.identity);    //폭발 이팩트
             Destroy(explosion);
             Destroy(boss1);
         }
@@ -118,13 +118,13 @@ public class Boss_1 : MonoBehaviour
             }
             if (angle < 2)
             {
-                //발사 각도
+                //발사 각도 변화
                 angle += shotAngle;
             }
 
             if (rotate == 0)
             {
-                //각속도 증가
+                //탄 생성각 증가
                 shotAngle += shotAngleRate;
                 shotAngleRate += shotAngleRate / 500;
                 if (shotAngle > 0.15)
@@ -135,7 +135,7 @@ public class Boss_1 : MonoBehaviour
 
             if (rotate == 1)
             {
-                //각속도 감소
+                //탄 생성각 감소
                 shotAngle -= shotAngleRate;
                 shotAngleRate -= shotAngleRate / 500;
                 if (shotAngle < 0.05)
@@ -144,25 +144,21 @@ public class Boss_1 : MonoBehaviour
                 }
             }
 
-            yield return new WaitForSeconds(0.015f);
+            yield return new WaitForSeconds(0.018f);
         } while (true);
     }
 
     IEnumerator BulletSpell3()
     {
         Vector2 v;
-        float digree;
         do
         {
             obj = (GameObject)Instantiate(bullet, boss1.transform.position, Quaternion.identity);
 
-            //플레이어가 있던 위치로 탄 발사
-            v = plane.transform.position - boss1.transform.position;
+            v = plane.transform.position - boss1.transform.position;    //보스->플레이어 방향
             obj.GetComponent<Rigidbody2D>().AddForce(v.normalized * speed);
-            yield return new WaitForSeconds(0.25f);
+
+            yield return new WaitForSeconds(0.29f);
         } while (true);
     }
 }
-
-
-
