a260bf09951f25f3bd24221b59ce78cd0591fadc
DESKTOP-KL9TUT2\최원준 <hhh9597h@naver.com>
2017-11-28 15:42:36 +0900

    update(배경추가, 적과 본체 충돌처리)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,10 +12,15 @@ public class Player : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D col)
     {
         //Player Missile 태그를 가진 오브젝트와 충돌시 오브젝트 삭제
-        if (col.CompareTag("Bullet"))
+        if (col.CompareTag("Bullet") || col.CompareTag("Enemy"))
         {
             Debug.Log("불릿과 충돌");
+<<<<<<< HEAD
+            Destroy(col.gameObject);
+            Destroy(this.gameObject);
+=======
             GameControll.health -= 1;
+>>>>>>> c8c828cae5f9d3007bca41e320785079da0f9a56
         }
     }
 
