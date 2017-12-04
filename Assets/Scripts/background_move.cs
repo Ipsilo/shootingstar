0c25d7aa7058de39c9637f8fcd23250343d70028
DESKTOP-KL9TUT2\최원준 <hhh9597h@naver.com>
2017-12-04 15:15:45 +0900

    주석추가

--- Assets/Scripts/background_move.cs
+++ Assets/Scripts/background_move.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class background_move : MonoBehaviour {
-    public float ScrollSpeed = 1.0f;
-    float Offset;
-    private new Renderer renderer;
+    public float ScrollSpeed = 1.0f;    //배경속도 값 저장변수
+    float Offset;                       //배경이동 좌표값 저장변수
+    private new Renderer renderer;      
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +14,7 @@ public class background_move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Offset += Time.deltaTime * ScrollSpeed;
-        renderer.material.mainTextureOffset = new Vector2(0, Offset);
+        Offset += Time.deltaTime * ScrollSpeed; //매 프레임마다 배경의 좌표를 움직여준다
+        renderer.material.mainTextureOffset = new Vector2(0, Offset);   //배경의 좌표에 y축으로 offset만큼 이동한 좌표를 계속 대입
 	}
 }
--- Assets/Scripts/random_spawn.cs
+++ Assets/Scripts/random_spawn.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class random_spawn : MonoBehaviour {
 
-    public GameObject Enemy; //Prefab을 받을 public 변수 입니다.
+    public GameObject Enemy; //Prefab을 받을 public 변.
                            
     void Start()
     {
-        InvokeRepeating("SpawnEnemy", 1.5f, 0.5f); //1.5초후 부터, SpawnEnemy함수를 1.5초마다 반복해서 실행 시킵니다.
+        InvokeRepeating("SpawnEnemy", 1.5f, 0.5f); //1.5초후 부터, SpawnEnemy함수를 1.5초마다 반복해서 실행
     }
     void Update()
     {
@@ -18,8 +18,8 @@ public class random_spawn : MonoBehaviour {
     void SpawnEnemy()
     {
         Vector3 position;
-        position = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 1);
-        position = transform.TransformPoint(position * .5f);
-        Instantiate(Enemy, position, transform.rotation);
+        position = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 1); //랜덤좌표생성
+        position = transform.TransformPoint(position * .5f); //로컬좌표를 월드좌표로 변환
+        Instantiate(Enemy, position, transform.rotation); //생성
     }
 }
